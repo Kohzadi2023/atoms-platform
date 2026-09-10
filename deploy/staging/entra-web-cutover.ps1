8437c48d8c6c4d9e682f5c1c048414e81ec56455
Kohzadi2023 <149624604+Kohzadi2023@users.noreply.github.com>
2026-09-09 23:45:15 -0400

    ops(web): pass External ID tenant to canonical build

--- deploy/staging/entra-web-cutover.ps1
+++ deploy/staging/entra-web-cutover.ps1
@@ -215,6 +215,7 @@ try {
             --build-arg "NEXT_PUBLIC_CONTROL_API_URL=$controlApiOrigin" `
             --build-arg "NEXT_PUBLIC_ENTRA_CLIENT_ID=$ExpectedWebClientId" `
             --build-arg "NEXT_PUBLIC_ENTRA_AUTHORITY=$ExpectedAuthority" `
+            --build-arg "NEXT_PUBLIC_ENTRA_TENANT_ID=$ExpectedTenantId" `
             --build-arg "NEXT_PUBLIC_ENTRA_API_SCOPE=$ExpectedApiScope" `
             --build-arg "NEXT_PUBLIC_STORAGE_ORIGIN=$controlApiOrigin" `
             --build-arg "NEXT_PUBLIC_PREVIEW_BASE_DOMAIN=$ExpectedPreviewBaseDomain" `
